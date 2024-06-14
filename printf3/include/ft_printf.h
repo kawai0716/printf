a0b4cef22a659f788e4b2e8408ed9941703b50b2
kawai <139425434+kawai0716@users.noreply.github.com>
2024-06-14 15:06:24 +0900

    Add files via upload

---
+++ printf3/include/ft_printf.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syuto <syuto@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 23:09:43 by syuto             #+#    #+#             */
+/*   Updated: 2024/06/14 15:03:32 by syuto            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <limits.h>
+# include <stdarg.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+#include "libft.h"
+
+int		ft_printf(const char *format, ...);
+int		ft_printf_di(int i, int *n);
+int		ft_printf_p(void *c, int *n);
+int		ft_printf_u(unsigned int i, int *n);
+int		ft_printf_x(unsigned long int i, int *n);
+int		ft_printf_x2(unsigned long int i, int *n);
+int		ft_putchar(char c);
+int		ft_putstr(char *str, int *n);
+
+#endif
